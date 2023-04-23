9d128a0ecd1557ee4457d892bf124f19cbfc3231
whoaam1 <yaroslav.shchudlyk.knm.2020@lpnu.ua>
2023-04-24 01:08:59 +0300

    Refacor authentication app

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
